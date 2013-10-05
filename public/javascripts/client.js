c1e45b5d9b006fbd40da8028b990e0671b8264ae
Pierre-Jean LEGER <leger.pierrejean@orange.fr>
2013-10-05 15:37:35 +0200

    Improved buttons and add alerts at the end of the game

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
 	var team = 0;
-	var socket = io.connect('http://localhost:3000');
 
+	$("#bluealert").hide();
+	$("#redalert").hide();
+
+	var socket = io.connect('http://localhost:3000');
 	socket.emit('connection');
 
 	socket.on('autobalancing', function(data) {
 		team = data.team;
 		if (data.team == 1) {
 			$("#team1").attr('disabled', false);
-			$("#team1").html('+1 Team 1');
+			$("#team1").html('<span class="glyphicon glyphicon-plus-sign"></span> Team 1');
 			$("#team2").attr('disabled', false);
-			$("#team2").html('-1 Team 2');
+			$("#team2").html('<span class="glyphicon glyphicon-minus-sign"></span> Team 2');
 			$("#lead").html("You are BLUE !");
 		} else {
 			$("#team1").attr('disabled', false);
-			$("#team1").html('-1 Team 1');
+			$("#team1").html('<span class="glyphicon glyphicon-minus-sign"></span> Team 1');
 			$("#team2").attr('disabled', false);
-			$("#team2").html('+1 Team 2');
+			$("#team2").html('<span class="glyphicon glyphicon-plus-sign"></span> Team 2');
 			$("#lead").html("You are RED !");
 		}
 	});
@@ -29,10 +32,24 @@ $(document).ready(function() {
 	socket.on('refreshteam', function(data) {
 		$("#nbTeam1").html(data.team1+" players");
 		$("#nbTeam2").html(data.team2+" players");
+
 	});
 
 	socket.on('end', function(data) {
-		
+		if(data.win == 1)
+		{
+			$("#bluealert").show();
+			setTimeout(function() {
+			$("#bluealert").hide();
+			}, 3000);
+		}
+		else
+		{
+			$("#redalert").show();
+			setTimeout(function() {
+			$("#redalert").hide();
+			}, 3000);
+		}
 	});
 
 	$("#team1").click(function(event) {
